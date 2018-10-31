ebe01eb8406b1d8ee8502f1a7963946dacda1c57
asdfv <grodno.by@mail.ru>
2018-10-31 22:22:50 +0300

    [Lession 8 - 9]  Add firebase authorization, downgrade min sdk, add login activity

--- app/src/main/java/by/grodno/vasili/instaclone/HomeActivity.kt
+++ app/src/main/java/by/grodno/vasili/instaclone/HomeActivity.kt
@@ -1,10 +1,13 @@
 package by.grodno.vasili.instaclone
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import com.google.firebase.auth.FirebaseAuth
 
 class HomeActivity : BaseActivity() {
     private val TAG = this::class.java.simpleName
+    private lateinit var mAuth: FirebaseAuth
 
     override fun getMenuIndex(): Int {
         return 0
@@ -15,5 +18,28 @@ class HomeActivity : BaseActivity() {
         setContentView(R.layout.activity_home)
         setupBottomNavigation()
         Log.d(TAG, "onCreate")
+
+
+        mAuth = FirebaseAuth.getInstance()
+        mAuth.signOut()
+//        auth.signInWithEmailAndPassword("it@grodno.net", "password").addOnCompleteListener {
+//            handleResult(it)
+//        }
     }
+
+    override fun onStart() {
+        super.onStart()
+        if (mAuth.currentUser == null) {
+            startActivity(Intent(this, LoginActivity::class.java))
+            finish()
+        }
+    }
+
+//    private fun handleResult(result: Task<AuthResult>) {
+//        if (result.isSuccessful) {
+//            Log.d(TAG, "SignIn success")
+//        } else {
+//            Log.d(TAG, "SignIn failure: ${result.exception}")
+//        }
+//    }
 }
